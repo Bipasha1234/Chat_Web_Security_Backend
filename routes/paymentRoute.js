42fd8da84525d3456a1aa232dbaed90f53bdd181
bipasha <bipashalamsal@gmail.com>
2025-07-26 20:45:01 +0545

    in create payment intent for paymentroute added

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -2,16 +2,13 @@ const express = require("express");
 const router = express.Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const Tip = require("../model/payment"); 
-
-
-
 router.post("/create-payment-intent", async (req, res) => {
   const { amount, tipperId, receiverId } = req.body;
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(amount * 100), // $5.00 becomes 500
-      currency: "usd",
+      amount: Math.round(amount * 100),
+      currency: "npr",
       metadata: {
         tipperId,
         receiverId,
@@ -24,6 +21,7 @@ router.post("/create-payment-intent", async (req, res) => {
     res.status(500).json({ error: "Failed to create payment intent" });
   }
 });
+
 
 router.post("/save-tip", async (req, res) => {
   const { tipperId, receiverId, amount, messageId } = req.body;
