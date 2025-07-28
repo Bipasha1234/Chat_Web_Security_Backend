6e16ef6c2b8d4e9e233a74d5a05f1d2fb26f670f
bipasha <bipashalamsal@gmail.com>
2025-07-28 18:37:43 +0545

    admin side code added and admin created

--- model/admin.js
+++ model/admin.js
@@ -1,33 +1,29 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
-const Credential = require("../models/credential");
+const { connect, disconnect } = require("mongoose");
+const Credential = require("../model/credential");
 const bcrypt = require("bcrypt");
 
-// Use .env values
 const { MONGO_URI, ADMIN_EMAIL, ADMIN_PASSWORD } = process.env;
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 const createAdmin = async () => {
   try {
+    await connect(MONGO_URI);
+
     const hashedPassword = await bcrypt.hash(ADMIN_PASSWORD, 10);
 
     const admin = new Credential({
       email: ADMIN_EMAIL,
       fullName: "Admin User",
       password: hashedPassword,
-      role: "admin", // Securely assign admin
+      role: "admin",
     });
 
     await admin.save();
-    console.log(" Admin created successfully");
+    console.log("Admin created successfully");
   } catch (err) {
-    console.error(" Failed to create admin:", err.message);
+    console.error("Failed to create admin:", err.message);
   } finally {
-    mongoose.disconnect();
+    await disconnect();
   }
 };
 
